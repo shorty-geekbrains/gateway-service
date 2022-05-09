8b99a587b2f02b18d6fbaefdbc3afdde689ffdc1
KorEugene <johnkorch@yandex.ru>
2022-05-09 19:19:13 +0300

    1) cleaned repo 2) added .gitigonre 3) gateway changed from Zull to Spring Cloud 4) cleaned pom.xml, changed spring boot version to 2.6.7 5) modified profiles

--- src/main/java/ru/geekbrains/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/ru/geekbrains/gatewayservice/GatewayServiceApplication.java
@@ -2,20 +2,11 @@ package ru.geekbrains.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import ru.geekbrains.gatewayservice.filter.SimpleFilter;
 
-@EnableZuulProxy
 @SpringBootApplication
 public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
-    }
-
-    @Bean
-    public SimpleFilter simpleFilter() {
-        return new SimpleFilter();
     }
 }
